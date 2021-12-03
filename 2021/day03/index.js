810c3a2ca8c2b9ba3d81df90f2f9ad9a516f6c7c
ctborg <coryborg@gmail.com>
2021-12-03 05:31:52 -0500

    Update index.js
    fetch

--- 2021/day03/index.js
+++ 2021/day03/index.js
@@ -1,1 +1,13 @@
+let day = 3;
+
+let fetchInput = async () => {
+  let resp = await fetch(`https://adventofcode.com/2021/day/${day}/input`);
+  return resp.text();
+}
+
+let input = await fetchInput();
+
+let parse = (i) => {
+  return i.trim().split(/\r?\n/);
+}
 
